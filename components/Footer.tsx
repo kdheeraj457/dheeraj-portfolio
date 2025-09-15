cc7450b00e6113ff54d0ca603e67d0bd74b36abd
kdheeraj457 <167636643+kdheeraj457@users.noreply.github.com>
2025-09-15 09:15:50 +0530

    Update Footer.tsx mail id change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:dheerajsinghkarki9@gmail.com">
+        <a href="mailto:dheerajworkk@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}

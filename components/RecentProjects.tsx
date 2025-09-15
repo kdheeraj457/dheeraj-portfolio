45f41491365dd02c813a56d60097511d6ea7b4ce
kdheeraj457 <dheerajsinghkarki9@gmail.com>
2025-09-15 09:08:58 +0530

    in recent project file commented bgimg div

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -18,17 +18,14 @@ const RecentProjects = () => {
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
             key={item.id}
           >
-            <PinContainer
-              title="/ui.aceternity.com"
-              href="https://"
-            >
+            <PinContainer title="/ui.aceternity.com" href="https://">
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                <div
+                {/* <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
                 >
                   <img src="/bg.png" alt="bgimg" />
-                </div>
+                </div> */}
                 <img
                   src={`${item.img}`}
                   // src={`dheeraj-portfolio${item.img}`}
@@ -67,7 +64,7 @@ const RecentProjects = () => {
                   ))}
                 </div> */}
 
-{/*                 <div className="flex justify-center items-center">
+                {/*                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
                     Check Live Site
                   </p>
